8cba05ff8d3953d7bebcdb2e1e9a37887e40d9da
Nicolas Kauan <166877999+NicolasKauan@users.noreply.github.com>
2026-03-22 23:49:57 -0300

    first commit

---
+++ src/main/java/com/nika/ex3_lista_javaspringboot/Ex3ListaJavaspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.nika.ex3_lista_javaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex3ListaJavaspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex3ListaJavaspringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nika/ex3_lista_javaspringboot/Ex3ListaJavaspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nika.ex3_lista_javaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex3ListaJavaspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
